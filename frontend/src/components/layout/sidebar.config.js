4d3c03249a20e46b344086aca263d3fb88d3d74b
sarairatni <ms_iratni@esi.dz>
2026-05-08 23:38:14 +0100

    merge fixed, formulaire fiche pilote

--- frontend/src/components/layout/sidebar.config.js
+++ frontend/src/components/layout/sidebar.config.js
@@ -42,14 +42,22 @@ const chefProjetItems = [
 const piloteItems = [
   { label: "Tableau de bord", icon: LayoutDashboard, to: "/dashboard-pilote" },
   { label: "Organigramme", icon: GitBranch, to: "/organigramme" },
+  // {
+  //   label: "Gestion processus",
+  //   icon: ClipboardList,
+  //   children: [
+  //     { label: "Fiches de processus", icon: FileText, to: "/gestion-processus/fiches/nouveau" },
+  //   ],
+  // },
   {
-  label: "Cartographie",
-  icon: Map,
-  children: [
-    { label: "Processus", icon: Network, to: "/cartographie/processus" },
-    { label: "Interactions", icon: GitBranch, to: "/cartographie/interactions" },
-  ],
-},
+    label: "Cartographie",
+    icon: Map,
+    children: [
+      { label: "Processus", icon: Network, to: "/cartographie/processus" },
+      { label: "Interactions", icon: GitBranch, to: "/cartographie/interactions" },
+       { label: "Fiches de processus", icon: FileText, to: "/gestion-processus/fiches/nouveau" },
+    ],
+  },
   {
     label: "Planif et suivi",
     icon: Calendar,
